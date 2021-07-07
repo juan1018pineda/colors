288a804cab433ee6012e77a82ba4173b7c019c08
pineda.juan92 <pineda.juan92@gmail.com>
2021-07-06 21:03:08 -0500

    Code enhancements

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ let selectedColor = "red";
 
 function changeColor() {
     selectedColor = document.getElementById('color').value;
-    let clicked = event.target.innerHTML;
-    boxes[clicked-1].style.backgroundColor = selectedColor;
+    let clickedElement = event.target.innerHTML;
+    boxes[clickedElement-1].style.backgroundColor = selectedColor;
 }
